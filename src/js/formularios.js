96b6683c20dcc6aeaccb55d797e13198beaee611
andres <aebb001@gmail.com>
2020-05-03 09:22:54 -0400

    Terminando formularios, (eliminamos una parte html la de formularis, le devolvimos display al featuring css, quitamos nuestra funcion de showfeatuing) Va todo bien

--- src/js/formularios.js
+++ src/js/formularios.js
@@ -1,83 +1,8 @@
-console.log("Hola mundo");
-const noCambia = "Andres";
-
-let cambia = "@Andresebb"
-
-function cambiaNombre(nuevoNombre){
-    cambia = nuevoNombre
-}
-
-const getUser = new Promise(function(todoBien, todoMal){
-    //llamar a un api (simulacion)
-    setTimeout(function(){
-        //Luego de 3 seg se ejecuta
-        todoBien("Todo esta ahuevo");
-    }, 3000)
-})
-
-
-const getUserAll = new Promise(function(todoBien, todoMal){
-    //llamar a un api (simulacion)
-    setTimeout(function(){
-        //Luego de 3 seg se ejecuta
-        todoBien("Todo esta ahuevo 5");
-    }, 5000)
-})
-
-/*getUser
-    .then(function(){  //.then cuando la promesa sale bien
-        console.log("Toda esta ahuevo")
-    })
-
-    .catch(function(mensaje){ //.catch cuando la promesa sale mal
-        console.log(mensaje)
-    })*/
-
-    Promise.all([ 
-        getUser,
-        getUserAll,
-    ])
-
-    .then(function(mensaje){
-        console.log(mensaje)
-    })
-
-    .catch(function(mensaje){
-        console.log(mensaje)
-    })
-
-
-
-    $.ajax("https://randomuser.me/api", {
-    method: "GET", //metodo get para traer los datod
-    success: function(data){
-        console.log(data)
-    },
-    error: function(error){
-        console.log(error)
-    }
-}) 
-
-fetch("https://randomuser.me/api/wwew")
-
-    .then(function(response){ //este es del fetch
-        //console.log(response)
-        return response.json() //esto tambien devuelve una promesa
-    })
-            .then(function(user){ //este es del json
-                console.log("user", user.results[0].name.first + user.results[0].name.last)
-            })
-
-            .catch(function(){
-                console.log("algo Fallo")
-            });
-
-
 (async function load(){
     
     async function getData(url){
-        const response = await fetch(url) /*tiene que esperar que no acabe para ejecutar el next codigo*/ 
-        const info = await response.json(); //Aca esperara a que se ejecute response
+        const response = await fetch(url) 
+        const info = await response.json(); 
         return info;
     }
 
@@ -87,22 +12,39 @@ fetch("https://randomuser.me/api/wwew")
     const $featuringContainer = document.getElementById('featuring');
     $featuringClass = document.querySelector(".featuring");
 
-    function setAttributes($element, attributes) {  //Agregando elementos a html
+    function setAttributes($element, attributes) {  
     for (const attribute in attributes) {
         $element.setAttribute(attribute, attributes[attribute]);
         }
     }
 
-    function showFeaturing(){
+    /*function showFeaturing(){
         $featuringClass.style.display="block";
-    }
+    }*/
 
-    //Agregando el evento del submit
-    $form.addEventListener("submit", (event) => {  
-        event.preventDefault(); //Evitar que se actulice cada vez que lanzamos el submit
-        showFeaturing();
-        $home.classList.add("search-active");//agregamos la clase search active a home cuando haga el submit
-        const $loader = document.createElement('img');//crear doc html
+    const BASE_API = "https://yts.mx/api/v2/";
+
+    function featuringTemplate(peli){ //Template para que muestre la pelicula
+        return(
+            `
+            <div class="featuring">
+                <div class="featuring-image">
+                    <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
+                </div>
+                <div class="featuring-content">
+                    <p class="featuring-title">Pelicula encontrada</p>
+                    <p class="featuring-album">${peli.title}</p>
+                </div>
+            </div>
+            `
+        )
+    } 
+
+    $form.addEventListener("submit", async (event) => {  
+        event.preventDefault(); 
+       // showFeaturing();
+        $home.classList.add("search-active");
+        const $loader = document.createElement('img');
         setAttributes($loader, {
             src: 'src/images/loader.gif',
             height: 50,
@@ -110,6 +52,19 @@ fetch("https://randomuser.me/api/wwew")
         })
         $featuringContainer.append($loader);
         
+        //formData nos permite llenar ese formulario
+        const data = new FormData($form);// se le pasa un elemento html de form
+
+        //dice traeme las pelicula de esta url que tenga el nombre que se escribio en el buscador
+        const peli = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get("name")}`);
+        
+        // data.get("name")En html dentro de los elementos form debemos tener un name, 
+        //esto se hace para que nos traiga lo que tenemos en el buscador
+        
+        const HTMLString = featuringTemplate(peli.data.movies[0]) //invoca esta funcion y peli tiene un data 
+        //y dentro de data hay movies traeme el elemento 0 
+
+        $featuringContainer.innerHTML = HTMLString; //convertirlo en elemento html
     })
 
 
@@ -140,12 +95,10 @@ fetch("https://randomuser.me/api/wwew")
         }
     function addEventClick($element) {
         $element.addEventListener('click', () => {
-            // alert('click')
             showModal()
         })
         }
     function renderMovieList(list, $container) {
-        // actionList.data.movies
         $container.children[0].remove();
         list.forEach((movie) => {
             const HTMLString = videoItemTemplate(movie);
@@ -163,7 +116,6 @@ fetch("https://randomuser.me/api/wwew")
     const $animationContainer = document.getElementById('animation');
     renderMovieList(animationList.data.movies, $animationContainer);
 
-    // const $home = $('.home .list #item');
     const $modal = document.getElementById('modal');
     const $overlay = document.getElementById('overlay');
     const $hideModal = document.getElementById('hide-modal');
